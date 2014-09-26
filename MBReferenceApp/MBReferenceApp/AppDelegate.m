8e4b237d4d7f138e7374418fa740235566c73563
uumoss <uumoss@gmail.com>
2014-09-25 21:25:40 -0600

    added navigation controller

--- MBReferenceApp/MBReferenceApp/AppDelegate.m
+++ MBReferenceApp/MBReferenceApp/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,14 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    ViewController *viewController = [ViewController new];
+    UINavigationController *navigationController = [[UINavigationController alloc]initWithRootViewController:viewController];
+    
+    self.window.rootViewController = navigationController;
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    
     return YES;
 }
 
